ab74bd65f0af2f8842ee455efb5c34b5892daaef
lang.eth <49306747+langeth123@users.noreply.github.com>
2023-08-11 17:33:06 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ import asyncio
 import web3
 from eth_account import Account as acc
 import time
+import requests
 
 
 with open("data/secrets.txt") as file:
@@ -48,7 +49,41 @@ ABI = [
         ],
         "name": "purchase",
         "outputs": [],
-        "stateMutability": "nonpayable",
+        "stateMutability": "payable",
         "type": "function"
     }
 ]
+
+VALUES = {
+    "polygon"   : "matic",
+    "bsc"       : "bnb",
+    "optimism"  : "eth",
+    "avalanche" : "avax",
+    "arbitrum"  : "eth",
+    "linea"     : "eth" 
+}
+
+def req(url: str, **kwargs):
+    try:
+        resp = requests.get(url, **kwargs)
+        if resp.status_code == 200:
+            return resp.json()
+        else:
+            #logger.error("Bad status code, will try again")
+            pass
+    except Exception as error:
+        logger.error(f"Requests error: {error}")
+
+def get_ticker_price(ticker) -> float:
+    def __find__(ticker: str, rates: list):
+        for k in rates:
+            name = k.get("symbol")
+            if name == ticker.upper() + 'USDT':
+                return float(k.get("price"))
+    while True:
+        response = req("https://api.binance.com/api/v3/ticker/price")
+        if type(response) is list:
+            return __find__(ticker, response)
+        else:
+            print(f'Cant get response from binance, tring again...')
+            sleep(5)
